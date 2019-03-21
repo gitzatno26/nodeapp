8b2c25cbb1e76289c301342bdcae915b1e5717d0
gitzatno26 <41445275+gitzatno26@users.noreply.github.com>
2019-03-21 16:49:58 -0700

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -1,2 +1,1 @@
-#kubectl delete secret devop
-kubectl create secret docker-registry devop --docker-server=phx.ocir.io --docker-username='orasenatdoracledigital05/oracleidentitycloudservice/billy.zou@oracle.com' --docker-password='3TIDw8fEzNk:Lc{opM5d' --docker-email='billy.zou@oracle.com'
+kubectl create secret docker-registry devop --docker-server=RegionCode.ocir.io --docker-username='TenancyName/UserName' --docker-password='AuthToken' --docker-email='user@email.com'
